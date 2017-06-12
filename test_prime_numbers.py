d230fc4553b4bdbd61d74a240b46ff84b0c8f635
lynn-thira <lynn.thira@gmail.com>
2017-06-13 00:09:14 +0300

    A working function to generate prime numbers from 0 to n with asymptotic analysis

---
+++ test_prime_numbers.py
@@ -0,0 +1,44 @@
+"""Unit tests for prime_numbers module."""
+
+import unittest
+from actualprime_numbers import prime_numbers
+
+
+class TestPrimeGenerator(unittest.TestCase):
+
+    def test_five(self):
+        """Testing if prime numbers are correctly generated."""
+
+        self.assertEqual(prime_numbers(5), [2, 3, 5])
+
+    def test_prime_numbers(self):
+        """Testing if prime numbers are correctly generated."""
+
+        self.assertEqual(prime_numbers(20), [2, 3, 5, 7, 11, 13, 17, 19])
+
+
+    def test_one_hundred(self):
+        """Testing if prime numbers are correctly generated."""
+
+        self.assertEqual(prime_numbers(100),
+                         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+                          97])
+    def test_zero(self):
+        """Testing if zero is correctly determined not to be prime."""
+
+        self.assertEqual(prime_numbers(0), "Zero or One cannot be prime numbers.")
+
+    def test_one(self):
+        """"Testing if one is correctly determined not to be prime."""
+
+        self.assertEqual(prime_numbers(1), "Zero or One cannot be prime numbers.")
+
+    def test_only_positive(self):
+        """Testing if error returned if negative integers input."""
+
+        self.assertEqual(
+            prime_numbers(-1), "Not possible to generate prime numbers for integers less than 2.")
+
+
+if __name__ == "__main__":
+    unittest.main()
